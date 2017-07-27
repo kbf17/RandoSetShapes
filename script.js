95b399858f5dfab9a4cbd0b73c9df05d69fe53a7
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-07-27 16:34:35 -0500

    shows rectangle

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('Document Ready');
+
+})
+let shapesArea = document.getElementById('shapesArea');
 class Shapes {
     constructor(shape){
-        this.shape = shape;
+        this.object = document.createElement('div');
+        this.object.className = shape;
     }
-    describe() {
+    draw(){
+        shapesArea.appendChild(this.object)
+    }
+    describe(shape) {
         console.log('I am a ' + this.shape)
     }
 }
 
 class Rectangle extends Shapes {
     constructor(width, height){
-        super('Rectangle');
+        super('rectangle');
+        this.height = height;
+        this.width = width;
+        this.object.style.height = height;
+        this.object.style.width = width;
+    }
+    get area() {
+        return this.height * this.width;
     }
 }
 
-class Square extends Shapes {
-    constructor(sideLength){
-        super('Square');
-    }
-}
+// class Square extends Shapes {
+//     constructor(sideLength){
+//         super('Square');
+//     }
+// }
 
-class Circle extends Shapes {
-    constructor(radius){
-        super('Circle');
-    }
-}
+// class Circle extends Shapes {
+//     constructor(radius){
+//         super('Circle');
+//     }
+// }
 
-class Triangle extends Shapes {
-    constructor(height){
-        super('Triangle');
+// class Triangle extends Shapes {
+//     constructor(height){
+//         super('Triangle');
+//     }
+// }
+function  newRec() {
+        let height = document.getElementById('recHeight').value;
+        let width = document.getElementById('recWidth').value;
+        let rectangle = new Rectangle(width, height);
+        rectangle.draw();
+        console.log(rectangle);
+        console.log('hello');
+        rectangle.describe();
     }
-}
+
+document.getElementById('genRec').addEventListener('click', newRec)
+
